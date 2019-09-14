9b6b047db7e37453cbd46eb70492f8db55c7b5cd
Piotr Oleś <piotrek.oles@gmail.com>
2019-09-14 02:29:41 +0200

    refactor: use simpler approach in isAsyncStatus
    Instead of iterating over AsyncStatus object, prepare an array with all available values. It will make it bulletproof and environment independent.

--- src/isAsyncStatus.ts
+++ src/isAsyncStatus.ts
@@ -1,9 +1,11 @@
 import { AsyncStatus } from "./AsyncStatus";
 
-// don't use Object.values as it's not supported by older browsers and we don't want to import polyfill for that
-const ALL = Object.keys(AsyncStatus).map(
-  key => AsyncStatus[key as keyof typeof AsyncStatus]
-);
+const ALL = [
+  AsyncStatus.IDLE,
+  AsyncStatus.PENDING,
+  AsyncStatus.RESOLVED,
+  AsyncStatus.REJECTED
+];
 
 export function isAsyncStatus(candidate: any): candidate is AsyncStatus {
   return ALL.indexOf(candidate) !== -1;
